f95f69d1fcafcbc636a005f756df58c094c46757
mglittleton <michaelglittleton@gmail.com>
2019-01-26 16:30:33 -0500

    MVP complete
    registration and login endpoints created and token generation is exported from authenticate.js.

--- config/routes.js
+++ config/routes.js
@@ -1,32 +1,70 @@
-const axios = require('axios');
+const axios = require("axios");
+const bcrypt = require("bcryptjs");
+const knex = require("knex");
 
-const { authenticate } = require('../auth/authenticate');
+const { authenticate, generateToken } = require("../auth/authenticate");
+const knexConfig = require("../knexfile");
+const db = knex(knexConfig.development);
+
+const cl = console.log;
 
 module.exports = server => {
-  server.post('/api/register', register);
-  server.post('/api/login', login);
-  server.get('/api/jokes', authenticate, getJokes);
+  server.post("/api/register", register);
+  server.post("/api/login", login);
+  server.get("/api/jokes", authenticate, getJokes);
 };
 
 function register(req, res) {
-  // implement user registration
+  const creds = req.body;
+  creds.password = bcrypt.hashSync(creds.password, 12);
+  cl('creds', creds)
+  db("users")
+    .insert(creds)
+    .then(ids => {
+      const id = ids[0];
+      cl("id", id);
+      db("users")
+        .where("id", id)
+        .first()
+        .then(user => {
+          const token = generateToken(user);
+          cl('token', token)
+          res.status(201).json({ id: user.id, token });
+        })
+        .catch(err => {
+          res.status(500).send(err);
+        });
+    })
+    .catch(err => {
+      res.status(500).send(err);
+    });
 }
 
 function login(req, res) {
-  // implement user login
+  const creds = req.body;
+  db('users').where('username', creds.username).first().then(user => {
+    if (user && bcrypt.compareSync(creds.password, user.password)) {
+      const token = generateToken(user);
+      res.json({ message: `Welcome ${user.username}`, token });
+    } else {
+      res.status(401).send("Shove off, faker!")
+    }
+  }).catch(err => {
+    res.status(500).send(err)
+  })
 }
 
 function getJokes(req, res) {
   const requestOptions = {
-    headers: { accept: 'application/json' },
+    headers: { accept: "application/json" }
   };
 
   axios
-    .get('https://icanhazdadjoke.com/search', requestOptions)
+    .get("https://icanhazdadjoke.com/search", requestOptions)
     .then(response => {
       res.status(200).json(response.data.results);
     })
     .catch(err => {
-      res.status(500).json({ message: 'Error Fetching Jokes', error: err });
+      res.status(500).json({ message: "Error Fetching Jokes", error: err });
     });
 }
